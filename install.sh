8f6665535eb65a12779b5a0d876088e8b7203e3b
Simon Heisterkamp <simon@heisterkamp.dk>
2024-06-03 15:14:57 +0000

    Allow the caller to set a fixed version

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 # Note: we cannot assume we're running bash and use the set -euo pipefail approach.
 set -e
 
-VERSION="0.220.0"
+# Pull latest version from VERSION file if not set.
+if [ -z "${VERSION:-}" ]; then
+    VERSION=$(cat $(dirname $0)/VERSION)
+fi
+
 FILE="databricks_cli_$VERSION"
 
 # Include operating system in file name.
